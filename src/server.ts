22e807dd497809384a8ceff88ff084f7b290e0e0
Arifur Rahman <arif.bd08060@gmail.com>
2023-05-30 23:49:45 +0600

    setup

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
-main().catch(err => console.log(err));
+main().catch(err => console.log(err))
 
 async function main() {
-try{
-    await mongoose.connect(config.database_url as string);
-    console.log(`databse connect successfully`);
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`databse connect successfully`)
 
     app.listen(config.port, () => {
-        console.log(`Example app listening on port ${config.port}`)
-      })
-
-}catch(err){
-    console.log(`failed try again ${err}`);
-
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log(`failed try again ${err}`)
+  }
 }
-
-} 
